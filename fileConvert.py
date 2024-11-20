f073cf39d02f379244530c14e22bfca4c4833af5
kayegonzales <kaye@tedthomas.com>
2024-11-20 22:05:43 +0800

    app.run update

--- fileConvert.py
+++ fileConvert.py
@@ -165,4 +165,8 @@ def display_data():
         return jsonify({'error': 'Failed to handle data', 'details': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT from the environment
+    logger.info(f"Starting server on port {port}")  # Log the port number
+    app.run(debug=False, host='0.0.0.0', port=port)
+
